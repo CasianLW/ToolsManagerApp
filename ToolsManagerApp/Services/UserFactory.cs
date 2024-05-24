5e2a89c8f2a9235afce8d09c915dae6a4bccdbd1
CasianLW <ciorbacasian@gmail.com>
2024-05-24 22:47:29 +0200

    user factory helper / handler

---
+++ ToolsManagerApp/Services/UserFactory.cs
@@ -0,0 +1,21 @@
+﻿using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Services
+{
+    public class UserFactory
+    {
+        public UserFactory() { }
+
+        public User InstantiateUser(RoleEnum role, string name, string email, string password)
+        {
+            User user = new User
+            {
+                Role = role,
+                Name = name,
+                Email = email,
+                Password = password // Consider hashing this
+            };
+            return user;
+        }
+    }
+}
