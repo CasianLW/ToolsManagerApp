cbbc25bb032a466a0cfa8cc1c9a64fcc034abd61
CasianLW <ciorbacasian@gmail.com>
2024-05-25 20:29:39 +0200

    Define interface for tool repository with methods for CRUD operations.

---
+++ ToolsManagerApp/Repositories/IToolRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Repositories
+{
+    public interface IToolRepository
+    {
+        Task<IEnumerable<Tool>> GetAllToolsAsync();
+        Task<Tool> GetToolByIdAsync(string id);
+        Task AddToolAsync(Tool tool);
+        Task UpdateToolAsync(Tool tool);
+        Task DeleteToolAsync(string id);
+        Task<IEnumerable<Tool>> GetToolsByIdsAsync(IEnumerable<string> ids); 
+    }
+}
