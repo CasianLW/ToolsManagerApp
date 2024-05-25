b64003950d86ffd5e381fab9b412a0ffafacb58a
CasianLW <ciorbacasian@gmail.com>
2024-05-25 02:48:52 +0200

    ViewModel binding for user management: linked to UsersViewModel, handles interactions between the UI and ViewModel

--- ToolsManagerApp/Views/UserPage.xaml.cs
+++ ToolsManagerApp/Views/UserPage.xaml.cs
@@ -1,12 +1,13 @@
-// namespace MauiApp1;
-namespace ToolsManagerApp.Views
+using ToolsManagerApp.ViewModels;
 
+namespace ToolsManagerApp.Views
 {
-	public partial class UserPage : ContentPage
-{
-	public UserPage()
-	{
-		InitializeComponent();
-	}
-}
+    public partial class UserPage : ContentPage
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+            BindingContext = App.ServiceProvider.GetService<UsersViewModel>();
+        }
+    }
 }
