47c9af17b5d224db3eeaf217cea2c6687912b9db
CasianLW <ciorbacasian@gmail.com>
2024-05-25 02:49:06 +0200

    interface definition for user data operations

---
+++ ToolsManagerApp/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(string id);
+        Task<User> GetUserByEmailAndPasswordAsync(string email, string password);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(string id);
+        Task EnsureAdminUserAsync();
+    }
+}
