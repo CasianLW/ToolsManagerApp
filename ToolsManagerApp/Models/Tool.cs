d7ea1efe4ddb2a85d07a8ab056ab33f881394556
CasianLW <ciorbacasian@gmail.com>
2024-05-25 18:09:54 +0200

    updated interface of tools to match mongodb model

--- ToolsManagerApp/Models/Tool.cs
+++ ToolsManagerApp/Models/Tool.cs
@@ -1,16 +1,22 @@
-﻿namespace ToolsManagerApp.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ToolsManagerApp.Models
 {
     public class Tool
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
         public string Name { get; set; }
-        public int CategoryId { get; set; }
+        public string CategoryId { get; set; }
         public StatusEnum Status { get; set; }
-        public int UserAssignedId { get; set; }
+        public string UserAssignedId { get; set; }
         public string QRCode { get; set; }
 
         public void CheckStatus() { /* Implementation */ }
-        public void AssignToUser(int userId) { UserAssignedId = userId; }
-        public void RemoveFromUser() { UserAssignedId = 0; }
+        public void AssignToUser(string userId) { UserAssignedId = userId; }
+        public void RemoveFromUser() { UserAssignedId = null; }
     }
 }
