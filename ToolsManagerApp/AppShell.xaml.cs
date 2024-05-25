d6a629789e91a074bc369fd03f40d040f270f39b
CasianLW <ciorbacasian@gmail.com>
2024-05-25 20:28:15 +0200

    Implement navigation routes and logout functionality, binding logout command for user session management.

--- ToolsManagerApp/AppShell.xaml.cs
+++ ToolsManagerApp/AppShell.xaml.cs
@@ -1,14 +1,47 @@
 ﻿using ToolsManagerApp.Views;
+using ToolsManagerApp.Services;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ToolsManagerApp;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-		 // Register routes for navigation
+    public ICommand LogoutCommand { get; }
+
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // Register routes for navigation
         Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
         Routing.RegisterRoute(nameof(UserPage), typeof(UserPage));
-	}
+        Routing.RegisterRoute(nameof(ToolsPage), typeof(ToolsPage));
+        Routing.RegisterRoute(nameof(UserToolsPage), typeof(UserToolsPage));
+
+        LogoutCommand = new AsyncRelayCommand(OnLogoutAsync);
+        BindingContext = this;
+
+        Navigating += OnNavigating;
+    }
+
+    private async Task OnLogoutAsync()
+    {
+        UserSession.Instance.ClearSession();
+        await GoToAsync("//LoginPage");
+    }
+
+    private void OnNavigating(object sender, ShellNavigatingEventArgs e)
+    {
+        if (e.Target.Location.OriginalString == "//LoginPage")
+        {
+            FlyoutBehavior = FlyoutBehavior.Disabled;
+        }
+        else
+        {
+            FlyoutBehavior = FlyoutBehavior.Flyout;
+        }
+    }
 }
