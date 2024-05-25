3335c21142163e26c5460eeac00417fb9f6b36d4
CasianLW <ciorbacasian@gmail.com>
2024-05-25 20:33:03 +0200

    Interface for category repository methods for CRUD

---
+++ ToolsManagerApp/Repositories/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(string id);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(string id);
+
+    }
+}
