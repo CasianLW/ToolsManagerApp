ce4b41c5b8589297282081a50f2312195b2af3d5
CasianLW <ciorbacasian@gmail.com>
2024-05-25 20:30:44 +0200

    Implement singleton UserSession service to manage and clear user session data.

---
+++ ToolsManagerApp/Services/UserSession.cs
@@ -0,0 +1,36 @@
+﻿using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Services
+{
+    public class UserSession
+    {
+        private static UserSession _instance;
+        private static readonly object _lock = new object();
+
+        private UserSession() { }
+
+        public static UserSession Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new UserSession();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public string Email { get; set; }
+        public RoleEnum Role { get; set; }
+
+        public void ClearSession()
+        {
+            Email = string.Empty;
+            Role = RoleEnum.Employee; // Default role or you can set it to null if Role is nullable
+        }
+    }
+}
