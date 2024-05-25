177621ec6030356169a41f9b5bbded77f946451b
CasianLW <ciorbacasian@gmail.com>
2024-05-25 02:41:45 +0200

    Routing for login & userPage

--- ToolsManagerApp/AppShell.xaml.cs
+++ ToolsManagerApp/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace ToolsManagerApp;
+﻿using ToolsManagerApp.Views;
+
+namespace ToolsManagerApp;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+		 // Register routes for navigation
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(UserPage), typeof(UserPage));
 	}
 }
