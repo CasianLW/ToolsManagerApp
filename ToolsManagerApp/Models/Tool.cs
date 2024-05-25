747f40413d1a8ab200230d51ecf1f4201e72e05a
CasianLW <ciorbacasian@gmail.com>
2024-05-25 20:28:26 +0200

    Create Tool model with properties and methods to manage tool assignments and status.

--- ToolsManagerApp/Models/Tool.cs
+++ ToolsManagerApp/Models/Tool.cs
@@ -8,15 +8,25 @@ namespace ToolsManagerApp.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        
+
         public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
         public string CategoryId { get; set; }
+
         public StatusEnum Status { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
         public string UserAssignedId { get; set; }
+
         public string QRCode { get; set; }
+        public string Description { get; set; } 
+
 
         public void CheckStatus() { /* Implementation */ }
+
         public void AssignToUser(string userId) { UserAssignedId = userId; }
+
         public void RemoveFromUser() { UserAssignedId = null; }
     }
 }
