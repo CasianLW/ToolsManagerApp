2380faf5c893079017bbe41a83dd5cacb63469e2
CasianLW <ciorbacasian@gmail.com>
2024-07-01 03:35:52 +0200

    check consumable converter

---
+++ ToolsManagerApp/Helpers/IsConsumableConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using ToolsManagerApp.Models; 
+
+namespace ToolsManagerApp.Converters
+{
+    public class IsConsumableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Check if the passed object is an instance of Consumable
+            return value is Consumable;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Converting back is not supported.");
+        }
+    }
+}
