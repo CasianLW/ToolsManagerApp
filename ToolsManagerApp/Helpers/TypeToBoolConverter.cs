affdafb570ec5197918f0af665571021bfcbd4f3
CasianLW <ciorbacasian@gmail.com>
2024-07-01 03:35:59 +0200

    convert type to bool

---
+++ ToolsManagerApp/Helpers/TypeToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using ToolsManagerApp.Models;
+
+namespace ToolsManagerApp.Converters
+{
+    public class TypeToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Consumable;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
