a0e3e18197b37e0a54291063fceefe41d2a5bac8
CasianLW <ciorbacasian@gmail.com>
2024-07-01 03:36:07 +0200

    consumable model

---
+++ ToolsManagerApp/Models/Consumable.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ToolsManagerApp.Models
+{
+    public class Consumable : Tool
+    {
+        [BsonElement("initialValue")]
+        public int InitialValue { get; set; }
+
+        [BsonElement("maxValue")]
+        public int MaxValue { get; set; }
+
+        [BsonElement("currentValue")]
+        public int CurrentValue { get; set; }
+    }
+}
