10a181b72be2993eb584debd337691d1e887da09
CasianLW <ciorbacasian@gmail.com>
2024-07-01 04:15:31 +0200

    added consumable methods

--- ToolsManagerApp/Repositories/IToolRepository.cs
+++ ToolsManagerApp/Repositories/IToolRepository.cs
@@ -6,11 +6,20 @@ namespace ToolsManagerApp.Repositories
 {
     public interface IToolRepository
     {
+        // Tool methods
         Task<IEnumerable<Tool>> GetAllToolsAsync();
         Task<Tool> GetToolByIdAsync(string id);
         Task AddToolAsync(Tool tool);
         Task UpdateToolAsync(Tool tool);
         Task DeleteToolAsync(string id);
-        Task<IEnumerable<Tool>> GetToolsByIdsAsync(IEnumerable<string> ids); 
+        Task<IEnumerable<Tool>> GetToolsByIdsAsync(IEnumerable<string> toolIds);
+
+        // Consumable methods
+        Task<IEnumerable<Consumable>> GetAllConsumablesAsync();
+        Task<Consumable> GetConsumableByIdAsync(string id);
+        Task AddConsumableAsync(Consumable consumable);
+        Task UpdateConsumableAsync(Consumable consumable);
+        Task DeleteConsumableAsync(string id);
+        Task<IEnumerable<Consumable>> GetConsumablesByIdsAsync(IEnumerable<string> consumableIds);
     }
 }
